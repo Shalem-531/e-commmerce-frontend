6684600aa7de7ef2dda73f00567bbb4ee83a3783
shalem <shalembadugu@gmail.com>
2026-03-25 11:12:51 +0530

    Move orderpage to react

--- ecom/src/App.jsx
+++ ecom/src/App.jsx
@@ -3,12 +3,14 @@ import './App.css';
 import { CheckoutPage } from './pages/CheckoutPage';
 import { HomePage } from './pages/HomePage';
 import { Route,Routes } from 'react-router-dom';
+import { OrdersPage } from './pages/OrdersPage';
 function App() {
   return (
     <>
     <Routes>
       <Route path="/" element={  <HomePage />} />
       <Route path="checkout" element={<CheckoutPage/>}/>
+      <Route path="orders" element={<OrdersPage/>}/>
     </Routes>
 
     </>
