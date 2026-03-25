f0ce960e1fa365bc50cc165cc135beec06c3b54d
shalem <shalembadugu@gmail.com>
2026-03-25 12:45:24 +0530

    add server configuration 2

--- ecom/vite.config.js
+++ ecom/vite.config.js
@@ -6,10 +6,10 @@ export default defineConfig({
   plugins: [react()],
   server:{
     proxy:{
-      './api':{
+      '/api':{
         target:"http://localhost:3000"
       }, 
-      
+
       '/images':{
          target:"http://localhost:3000"
       }
