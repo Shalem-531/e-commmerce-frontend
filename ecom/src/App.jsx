fa9fccaf0c2a942eaa3c1d24cc26ce3883e4b351
shalem <shalembadugu@gmail.com>
2026-05-13 12:18:39 +0530

    Integrated authentication pages

--- ecom/src/App.jsx
+++ ecom/src/App.jsx
@@ -5,22 +5,70 @@ import { HomePage } from './pages/home/HomePage';
 import { Route,Routes } from 'react-router-dom';
 import { OrdersPage } from './pages/orders/OrdersPage';
 import { useEffect,useState } from 'react';
+import { Register } from './pages/register';
+import { Login } from './pages/login';
+import { ProtectedRoute } from './pages/protectedroute';
 function App() {
   const [cart,setCart]=useState([]);
+  const[user,setUser]=useState(null);
      const loadCart=async ()=>{
     const response=await axios.get('/api/cart-items?expand=product');
         setCart(response.data);
+       
     }
   useEffect(()=>{
     loadCart();
   },[])
+  useEffect(() => {
+
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    axios.defaults.headers.common["Authorization"] =
+      `Bearer ${token}`;
+  }
+
+}, []);
+
   return (
     <>
-    <Routes>
-      <Route path="/" element={  <HomePage cart={cart} loadCart={loadCart}/>} />
-      <Route path="checkout" element={<CheckoutPage cart={cart} loadCart={loadCart}/>}/>
-      <Route path="orders" element={<OrdersPage cart={cart}/>}/>
-    </Routes>
+   <Routes>
+
+  <Route path="/" element={<Register />} />
+
+  <Route path="/login" element={<Login setUser={setUser} />} />
+
+  {/* 🔒 Protected Home */}
+  <Route
+    path="/home"
+    element={
+      <ProtectedRoute>
+        <HomePage cart={cart} loadCart={loadCart} />
+      </ProtectedRoute>
+    }
+  />
+
+  {/* 🔒 Protected Checkout */}
+  <Route
+    path="/checkout"
+    element={
+      <ProtectedRoute>
+        <CheckoutPage cart={cart} loadCart={loadCart} />
+      </ProtectedRoute>
+    }
+  />
+
+  {/* 🔒 Protected Orders */}
+  <Route
+    path="/orders"
+    element={
+      <ProtectedRoute>
+        <OrdersPage cart={cart} />
+      </ProtectedRoute>
+    }
+  />
+
+</Routes>
 
     </>
   )
