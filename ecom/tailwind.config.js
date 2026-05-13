a053771785b72f79522c8c6515c0d01ab2e0ab81
shalem <shalembadugu@gmail.com>
2026-05-13 12:16:26 +0530

    Added tailwindcss to authentication page

---
+++ ecom/tailwind.config.js
@@ -0,0 +1,16 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+
+  corePlugins: {
+    preflight: false,
+  },
+
+  theme: {
+    extend: {},
+  },
+
+  plugins: [],
+};
