3cab3e5f9f71618c8ba3498bf4a6ab5481248d8e
shalem <shalembadugu@gmail.com>
2026-05-13 12:21:36 +0530

    Configured Vite settings

--- ecom/vite.config.js
+++ ecom/vite.config.js
@@ -4,15 +4,28 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    proxy:{
-      '/api':{
-        target:"http://localhost:3000"
-      }, 
 
-      '/images':{
-         target:"http://localhost:3000"
+  server: {
+    proxy: {
+
+      // 🔐 Auth backend
+      '/api/users': {
+        target: 'http://localhost:5000',
+        changeOrigin: true
+      },
+
+      // 🛒 Ecommerce backend
+      '/api': {
+        target: 'http://localhost:7000',
+        changeOrigin: true
+      },
+
+      // 🖼 Images
+      '/images': {
+        target: 'http://localhost:7000',
+        changeOrigin: true
       }
+
     }
   }
 })
